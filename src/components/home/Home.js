edd3d2e1cdba25249f749b8eb18367586d5e851a
balqeesHmoud <balqeeshmoud2@gmail.com>
2024-02-19 19:07:46 +0200

    class-16

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,16 +2,16 @@ import toursData from '../../data/dp.json'
 import Header from '../header/Header.js';
 import Tours from '../tours/Tours.js';
 import Footer from '../footer/Footer.js';
-function Home(){
+function Home() {
     console.log(toursData[0]);
-    return(
-        
+    return (
+
         <div className="home">
-        <Header/>
-        {toursData.map(element =><Tours toursName={element.name} toursImage={element.image} />)}
-        <Footer/>
+            <Header />
+            {toursData.map(element => <Tours toursName={element.name} toursImage={element.image} />)}
+            <Footer />
         </div>
-        
+
     );
 }
 export default Home;
